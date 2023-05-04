097d022919acd274d32b31b5a0fc2d9e7341dc34
Umesh9205kumar <132475260+Umesh9205kumar@users.noreply.github.com>
2023-05-04 15:39:40 +0530

    Add files via upload

---
+++ duplication.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class lokesh
+    {
+      static void Main()
+        {
+            Console.WriteLine("Enter you input");
+            string s = Console.ReadLine();
+            int cnt = 1;
+            for(int i=0; i<s.Length; i++)
+            {
+                for(int j=i+1; j<s.Length; j++)
+                {
+                    if (s[i] == s[j])
+                    {
+                        cnt++;
+                        Console.Write(s[i]+ " ");
+                    }
+                }
+
+            }
+            Console.Write("The char comes in string:" + cnt);
+            Console.ReadLine();
+        }
+    }
+}
