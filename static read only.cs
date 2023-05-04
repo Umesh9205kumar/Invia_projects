122eb2c7715b1bf063d0e136e62ed57ae486003e
Umesh9205kumar <132475260+Umesh9205kumar@users.noreply.github.com>
2023-05-04 23:06:45 +0530

    Add files via upload

---
+++ static read only.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class Class1
+    {
+        static readonly int var; //static reaonly var
+        static Class1() //static constructor
+        {
+            Class1.var = 200;
+        }
+       static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.var); //access var by class level
+            Console.ReadLine();
+        }
+    }
+}
