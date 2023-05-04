95945f21447599d76973e09a71689def8f3fd7bd
Umesh9205kumar <132475260+Umesh9205kumar@users.noreply.github.com>
2023-05-04 23:05:02 +0530

    Add files via upload

---
+++ static var.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int b = 10; //static variable 
+        static void Main(string[] args)
+        {
+            int a = Program.b; 
+            Console.WriteLine(a);   
+            Console.ReadLine();
+        }
+    }
+}
