63bb99da83f651231e2baf28fd11e31f25ff3433
Umesh9205kumar <132475260+Umesh9205kumar@users.noreply.github.com>
2023-05-04 23:10:01 +0530

    Add files via upload

---
+++ const.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   class class2
+    {
+        public const int var = 290;//initialize value only ones and it is universly fixed
+        static void Main(string[] args)
+        {
+            Console.WriteLine(class2.var);
+            Console.ReadLine();
+        }
+    }
+}
